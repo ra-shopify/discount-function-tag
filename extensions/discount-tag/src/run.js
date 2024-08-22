a92060f4855297109dbb798cd12c5a2076f3bb65
richad-abuani <richad.abuani@gmail.com>
2024-08-22 07:49:15 -0400

    Initial Commit

---
+++ extensions/discount-tag/src/run.js
@@ -0,0 +1,75 @@
+// @ts-check
+import { DiscountApplicationStrategy } from "../generated/api";
+
+// Use JSDoc annotations for type safety
+/**
+* @typedef {import("../generated/api").RunInput} RunInput
+* @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+* @typedef {import("../generated/api").Target} Target
+* @typedef {import("../generated/api").ProductVariant} ProductVariant
+*/
+
+/**
+* @type {FunctionRunResult}
+*/
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
+
+// The configured entrypoint for the 'purchase.product-discount.run' extension target
+/**
+* @param {RunInput} input
+* @returns {FunctionRunResult}
+*/
+export function run(input) {
+
+  const memberType = input.cart.buyerIdentity?.customer?.metafield?.value
+  let discountAmount = '0'
+
+  if(memberType === 'Bronze')
+    discountAmount = "5.0"
+
+  if(memberType === 'Silver')
+    discountAmount = "10.0"
+
+  if(memberType === 'Gold')
+    discountAmount = "15.0"
+    
+  const targets = input.cart.lines
+ // Inclue all cart lines as part of the discount target
+  targets.map(line => {
+    return /** @type {Target} */ ({
+      // Use the cart line ID to create a discount target
+      orderSubtotal: {
+        excludedVariantIds: [],
+      }
+    });
+  });
+
+  console.error(targets)
+
+  if (discountAmount === '0') {
+    // You can use STDERR for debug logs in your function
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  // The @shopify/shopify_function package applies JSON.stringify() to your function result
+  // and writes it to STDOUT
+  return {
+    discounts: [
+      {
+        // Apply the discount to the collected targets
+        targets,
+        // Define a percentage-based discount
+        value: {
+          percentage: {
+            value: discountAmount
+          }
+        }
+      }
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First
+  };
+};
